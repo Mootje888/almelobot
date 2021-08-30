6c02318f109eacdee496aad5be84520838cb666f
Mootje888 <86240655+Mootje888@users.noreply.github.com>
2021-08-30 11:31:26 +0200

    init
    init

---
+++ commands/info.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    var botEmbed = new discord.MessageEmbed()
+    .setTitle("Breda RP")
+    .setDescription("Breda is een RP game")
+    .setColor("#3e81ed")
+    .addFields(
+        {name: "Maker van de bot", value: "Mootje"},
+        {name: "Naam server", value: "Breda"},
+        {name: "Totaal Members", value: message.guild.memberCount}
+    )
+    .addField("Bot naam", client.user.username)
+    .setThumbnail("https://www.lavueltaholanda.com/uploads/_1040x585_crop_center-center_75_none/01-overzicht-Breda-met-Grote-Kerk-centraal-copyright-www.visitbreda.nl.jpg");
+
+
+    return message.channel.send(botEmbed);
+}
+
+
+module.exports.help = {
+    name: "info"
+}
---
+++ index.js
@@ -0,0 +1,81 @@
+// Main dingen
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+
+const fs = require("fs");
+
+
+const bot = new discord.Client();
+bot.commands = new discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err);
+
+    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+    if(jsFiles.length <= 0) {
+        console.log('Kon geen files vinden');
+        return;
+    }
+
+    jsFiles.forEach((f, i) => {
+
+        var fileGet = require(`./commands/${f}`);
+        console.log(`De file ${f} is geladen`);
+
+        bot.commands.set(fileGet.help.name, fileGet);
+
+
+    })
+
+})
+
+bot.login(botConfig.token);
+ 
+bot.on("ready", async () => {
+ 
+    console.log(`${client.user.username} is online.`);
+ 
+    client.user.setActivity("Mensen die commando's uitvoeren", { type: "WATCHING" });
+ 
+});
+ 
+ 
+bot.on("message", async message => {
+ 
+    if(message.author.bot) return;
+ 
+    if(message.channel.type === "dm") return;
+ 
+    var prefix = botConfig.prefix;
+ 
+    var messageArray = message.content.split(" ");
+ 
+    var command = messageArray[0];
+
+
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if(commands) commands.run(bot,message, arguments);
+
+});
+
+   // Rank Burger als iemand joint
+
+   bot.on("guildMemberAdd", member => {
+
+    var role = member.guild.roles.cache.get('844948731626258432');
+
+    if(!role) return;
+
+    member.roles.add(role);
+
+    var channel = member.guild.channels.cache.get('856510455652679710');
+
+    if(!channel) return;
+
+    channel.send(`Welkom in de server ${member}! Heb een fijne tijd!!`);
+
+   });
